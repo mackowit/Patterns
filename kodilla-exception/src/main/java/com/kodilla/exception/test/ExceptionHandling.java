e0d6cd75bb66ba14a26bbeb66cccb9d8ac969313
mackowit <mackowit@o2.pl>
2019-09-25 10:54:01 +0200

    8.3 Second Challenge

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+   public static void main(String[] args) {
+            SecondChallenge secondChallenge = new SecondChallenge();
+            try {
+                System.out.println(secondChallenge.probablyIWillThrowException(2, 1.5));
+            } catch (Exception e) {
+                System.out.println("Method throwed an exception");
+            } finally {
+                System.out.println("Second challenge done!!");
+            }
+        }
+}
