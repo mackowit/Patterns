95374595dd653d751365103ae185ba6975faf79c
mackowit <mackowit@o2.pl>
2020-01-27 08:29:52 +0100

    14.3 portfolio v.2

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,4 +1,21 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //When
+        board.toDoList.tasks.add("Task to do");
+        board.inProgressList.tasks.add("Tak in progress");
+        board.doneList.tasks.add("Task done");
+        //Then
+        board.getTasksFromBoard();
+
+    }
 }
